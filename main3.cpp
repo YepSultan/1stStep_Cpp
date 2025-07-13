66c8bd7ba0078e998f56e887a1e9ad57d50f9ac0
YepSultan <evening.tea@mail.ru>
2025-07-13 20:03:46 +0500

    Work with Classes and add new features

--- main3.cpp
+++ main3.cpp
@@ -1,33 +1,56 @@
 #include <iostream>
-#include <string>
-using std::cin;
-using std::cout;
-using std::endl;
+#include <string> 
+
 using namespace std;
-#define nl endl
-#define out cout
+
 #define in cin
+#define out cout
+#define nl endl
 
-void nicknames(){
-	string name, name2, name3;
-	out << "Input Name of 1st Person: ";
-	in >> name;
-	out << "Input Name of 2nd Person: ";
-	in >> name2;
-	out << "Input Name of 3th Person: ";
-	in >> name3;
+string name, name2, name3;
 
-	out << "1st: " << name << nl;
-	out << "2nd: " << name2 << nl;
-	out << "3rd: " << name3 << nl;
+class PersonNames{
+public:
+	void getNames(){
+		out << "Input name: ";
+		in >> name;
+		out << "Input name2: ";
+		in >> name2;
+		out << "Input name3: ";
+		in >> name3;
 
- 
-}
 
-void ages(){
-	out << "Input ages "
-}
+
+		out << "1st Client: " << name  << nl;
+		out << "2nd Client: " << name2 << nl;
+		out << "3rd Client: " << name3 << nl;
+
+	} 
+};
+
+class PersonAges{
+public:
+	int age, age2, age3;
+	void getAges(){
+		out << "Input age: ";
+		in >> age;
+		out << "Input age2: ";
+		in >> age2;
+		out << "Input age3: ";
+		in >> age3;
+
+		out << "| 1st: " << name  << " | Age: " << age  << nl;
+		out << "| 2nd: " << name2 << " | Age: " << age2 << nl;
+		out << "| 3rd: " << name3 << " | Age: " << age3 << nl; 
+
+
+	}
+};
 
 int main(){
-	nicknames();
+	PersonNames pn;
+	pn.getNames();
+
+	PersonAges pa;
+	pa.getAges();
 }
